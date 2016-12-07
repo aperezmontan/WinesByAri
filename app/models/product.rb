31d116196e4df7e561a48b19a61a4f1434d2af89
Ari Perez <aris.a.perez@gmail.com>
2016-12-06 22:56:11 -0700

    Sets up Product model

---
+++ app/models/product.rb
@@ -0,0 +1,11 @@
+class Product
+  include Mongoid::Document
+  field :external_id, type: Integer
+  field :name, type: String
+  field :url, type: String
+  field :price_min, type: BigDecimal
+  field :price_max, type: BigDecimal
+  field :price_retail, type: BigDecimal
+  field :type, type: String
+  field :year, type: String
+end
