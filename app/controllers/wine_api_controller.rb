57be22c9df52c825183490d13fabdd63414be0de
Ari Perez <aris.a.perez@gmail.com>
2016-12-09 08:11:02 -0700

    Adds AJAX call to load data from wine API site

--- app/controllers/wine_api_controller.rb
+++ app/controllers/wine_api_controller.rb
@@ -1,8 +1,10 @@
 class WineApiController < ApplicationController
+  protect_from_forgery with: :null_session
+
   # POST /request_data
   def request_data
     @response = client.request({})
-
+binding.pry
     ::Product.load_api_data(@response.body)
 
     render :nothing => true, :status => @response.status
