a0f933abe1d8567b197820d5b5b9e9454caf3330
Ari Perez <aris.a.perez@gmail.com>
2016-12-10 15:51:51 -0700

    Implements pagination

--- app/assets/javascripts/components/app.js.jsx
+++ app/assets/javascripts/components/app.js.jsx
@@ -1,8 +1,74 @@
-// import "bootstrap/min/"
-
 var App = React.createClass ({
   getInitialState: function(){
-    return { search: '' };
+    return {
+      currentPage: 1,
+      defaultPaginationValue: 2,
+      lastPage: Math.ceil(this.props.products.length/2),
+      productCount: this.props.products.length,
+      productsPerPage: '',
+      search: '',
+    };
+  },
+  deleteDataAjax: function(){
+    var xhr = new XMLHttpRequest(),
+    method = "POST",
+    url = "/delete_data";
+
+    xhr.open(method, url, true);
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+          console.log(xhr.responseText);
+      }
+    };
+    xhr.send();
+  },
+  loadDataAjax: function(){
+    var xhr = new XMLHttpRequest(),
+    method = "POST",
+    url = "/request_data";
+
+    xhr.open(method, url, true);
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
+          console.log(xhr.responseText);
+      }
+    };
+    xhr.send();
+  },
+  nextPage: function(){
+    if (this.state.currentPage == this.state.lastPage){
+      return nil
+    } else {
+      this.setState({
+        currentPage: this.state.currentPage + 1
+      })
+    }
+  },
+  newProduct: function(){
+    window.location.href = "/products/new";
+  },
+  previousPage: function(){
+    console.log("Current Page", this.state.currentPage);
+    if (this.state.currentPage == 1){
+      return nil
+    } else {
+      this.setState({
+        currentPage: this.state.currentPage - 1
+      })
+    }
+  },
+  rowClick: function(){
+    this.setState({
+      clicked: !this.state.clicked,
+    })
+  },
+  updatePagination: function(e) {
+    this.setState({ productsPerPage: e.target.value });
+    if (!e.target.value){
+      this.setState({ lastPage: Math.ceil(this.state.productCount/2) })
+    } else {
+      this.setState({ lastPage: Math.ceil(this.state.productCount/e.target.value)})
+    }
   },
   updateSearch: function(e) {
     this.setState({ search: e.target.value });
@@ -15,6 +81,7 @@ var App = React.createClass ({
           search_string = Object.values(product).filter((el) => { return el != null; }).join("")
           return search_string.toLowerCase().indexOf(this.state.search.toLowerCase()) != -1;
         }
+
       );
     } else {
       filteredProducts = this.props.products;
@@ -23,11 +90,37 @@ var App = React.createClass ({
     // else if ( this.state.search.indexOf(":") != -1){
     //   searchObjectString = '{' + this.state.search + '}'
     // }
+
+    // PAGINATION
+    if (!this.state.productsPerPage){
+      paginatedProducts =  filteredProducts.slice(2 * (this.state.currentPage - 1), 2 * this.state.currentPage)
+    } else {
+    console.log("Calculating pagination");
+      paginatedProducts =  filteredProducts.slice(this.state.productsPerPage * (this.state.currentPage - 1), this.state.productsPerPage * this.state.currentPage)
+    }
+
     return (
       <div>
+        <div className="apiDataButtons">
+          <button data-disable-with="Please wait..." name="button" onClick={this.loadDataAjax} type="submit">Load API Data !!</button>
+          <button data-disable-with="Please wait..." name="button" onClick={this.deleteDataAjax} type="submit">Delete API Data</button>
+        </div>
+        <div>
+          <button data-disable-with="Please wait..." name="button" onClick={this.previousPage} type="submit">Previous</button>
+          <h4>Page {this.state.currentPage} of {this.state.lastPage}</h4>
+          <button data-disable-with="Please wait..." name="button" onClick={this.nextPage} type="submit">Next</button>
+        </div>
+
         <h3>Products</h3>
 
-        <input className='form-control' type='text' onChange={this.updateSearch} value={this.state.search} placeholder='Search Products'/>
+        <button data-disable-with="Please wait..." name="button" onClick={this.newProduct} type="submit">New Product</button>
+        <div>
+          <label for="paginationControl">Products per page</label>
+          <input className='form-control' type='text' id="paginationControl" onChange={this.updatePagination} value={this.state.productsPerPage} placeholder='2'/>
+        </div>
+        <div>
+          <input className='form-control' type='text' onChange={this.updateSearch} value={this.state.search} placeholder='Search Products'/>
+        </div>
 
         <table className="table">
           <thead>
@@ -44,8 +137,8 @@ var App = React.createClass ({
             </tr>
           </thead>
           <tbody>
-            {filteredProducts.map((product) => {
-              return <Product product={product} key={product._id["$oid"]}/>
+            {paginatedProducts.map((product) => {
+              return [<Product product={product} onClick={this.rowClick} key={product._id["$oid"]}/>, <ProductDescription product={product} key={product._id["$oid"] + 'd'}/>]
             })}
           </tbody>
         </table>
@@ -53,5 +146,3 @@ var App = React.createClass ({
     );
   }
 })
-              // <th>Description</th>
-              // <th>Url</th>
--- app/assets/javascripts/components/product.js.jsx
+++ app/assets/javascripts/components/product.js.jsx
@@ -11,9 +11,12 @@ var Product = React.createClass ({
     year: React.PropTypes.string,
     id: React.PropTypes.string
   },
+  expandRow: function(){
+    alert("You clicked the row !");
+  },
   render () {
     return (
-      <tr>
+      <tr data-toggle="collapse" className="accordion-toggle">
         <td>{this.props.product.name}</td>
         <td className="text-center">{parseFloat(this.props.product.price_min).toFixed(2)}</td>
         <td className="text-center">{parseFloat(this.props.product.price_max).toFixed(2)}</td>
@@ -23,6 +26,20 @@ var Product = React.createClass ({
         <td className="text-center"><a href={'products/' + this.props.product._id["$oid"]}>#</a></td>
         <td className="text-center"><a href={'products/' + this.props.product._id["$oid"] + '/edit'}>#</a></td>
         <td className="text-center"><a data-confirm="Are you sure?" data-method="delete" href={'products/' + this.props.product._id["$oid"]} rel="nofollow">#</a></td>
+      </tr>
+    );
+  }
+})
+
+var ProductDescription = React.createClass({
+  render(){
+    return (
+      <tr>
+        <td className="hiddenRow">
+            <div className="accordian-body collapse">
+              <h1>Hi from the hiddenRow</h1>
+            </div>
+        </td>
       </tr>
     );
   }
