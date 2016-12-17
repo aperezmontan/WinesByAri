4f05c7fc1430f7290cf8a66e04af9a42b6f5e411
Ari Perez <aris.a.perez@gmail.com>
2016-12-17 07:40:20 -0700

    Changes binding config

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,7 +12,7 @@ set :use_sudo,        false
 set :stage,           :production
 set :deploy_via,      :remote_cache
 set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       %w(tcp://0.0.0.0:3003 http:///0.0.0.0:82)
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
